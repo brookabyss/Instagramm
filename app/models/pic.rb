03f2e9b032c1bf45e794c28b133f9eb3c742af74
Brook Kebede <brookabyss@gmail.com>
2016-10-07 07:38:39 +0000

    modified has_attached_file in Pic model to include new url

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,6 +2,12 @@ class Pic < ApplicationRecord
   acts_as_votable
   belongs_to :user
   
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, 
+  styles: { medium: "300x300>", thumb: "100x100>" },
+  default_url: "https://console.aws.amazon.com/s3/home?region=us-west-2#&bucket=instagram-rep&prefix="
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
+  
+  
+  
 end
